85dd9893b7fded4cd363eb6a8960a53f9e7324a3
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2025-12-16 11:48:38 +0530

    Fix test failure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        System.out.print("$ ");
         String input = sc.nextLine();
         System.out.print("\n$ "+input+"\n"+input+": command not found");
         sc.close();
