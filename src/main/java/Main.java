ba3df9846a1f8881e2c1bec401301fe073b6b630
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2025-12-18 09:27:25 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,36 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-
+/*
+- extract path
+- split path into all folders
+- extract all folders
+- recursively search in all folders
+- if found, display the path
+*/
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String input = "", command;
+        String input = "", command, path;
+        int i;
+        path = System.getenv("PATH");
+        String[] dirs = path.split(":");
+        List<Path> envPaths = new ArrayList<>();
+        Path pathObj, filePath;
+        for(i = 0; i < dirs.length; i++){
+            pathObj = Paths.get(dirs[i]);
+            if(Files.exists(pathObj)){
+                envPaths.add(pathObj);
+            }
+        }
+
         int idx;
+        boolean exeFound;
         Set<String> builtin = new HashSet<>();
         builtin.add("echo");
         builtin.add("exit");
@@ -15,12 +39,26 @@ public class Main {
             System.out.print("$ ");
             input = sc.nextLine();
             if(input.startsWith("type")){
+
+                exeFound = false;
                 idx = input.indexOf(' ') ;
                 command = input.substring(idx+1);
+
                 if(builtin.contains(command)){
                     System.out.println(command +" is a shell builtin");
                 } else {
-                    System.out.println(command +": not found");
+                    for(Path p : envPaths){
+                        filePath = p.resolve(command);
+                        if(Files.exists(filePath) && Files.isExecutable(filePath)){
+                            exeFound = true;
+                            System.out.println(command + " is "+filePath);
+                            break;
+                        }
+                    }
+
+                    if(!exeFound){
+                        System.out.println(command+": not found");
+                    }
                 }
             } else if(input.startsWith("echo")){
                 idx = input.indexOf(' ');
@@ -30,7 +68,6 @@ public class Main {
             } else {
                 System.out.println(input+": command not found");
             }
-            
         }
 
         sc.close();
